242f0faeeedc09b8748ec4a8cc5431c0e5c2a1c7
iddiaz <diazdiazivan@gmail.com>
2017-03-13 14:19:07 +0100

    pipes en Angular 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  nombre = 'Iván';
+  nombre2 = 'iVAn diAz dIAz';
+  arreglo: number[] = [1,2,3,4,5,6,7,8,9];
+  PI: number = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: '19'
+    }
+  }
+
+  valorDePromesa = new Promise ( (resolve, reject) => {
+    setTimeout( () => resolve ('Llegaron los resultados'), 3500);
+  });
+
+  fecha = new Date ();
+
+  // video: string = 'https://www.youtube-nocookie.com/embed/m_J9OViGkB8?controls=0';
+  video = 'm_J9OViGkB8?controls=0';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//Internacionalización
+import { LOCALE_ID } from '@angular/core';
+
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "es" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
